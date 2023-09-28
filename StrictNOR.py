cf41852f6f809f9329cbe0f40d6d38d6a3e3d5e7
aaronstevenson408 <64069763+aaronstevenson408@users.noreply.github.com>
2023-09-27 20:23:57 -0700

    Reorganization

--- StrictNOR.py
+++ StrictNOR.py
@@ -4,7 +4,8 @@ from torch.utils.data import DataLoader, Dataset
 import pickle
 import os
 import torch.nn as nn
-import matplotlib.pyplot as plt  # Import Matplotlib
+import matplotlib.pyplot as plt
+from sklearn.metrics import accuracy_score
 
 # Define the number of data points you want to generate
 num_data_points = 1000
@@ -30,7 +31,7 @@ class NORGateDataset(Dataset):
         for _ in range(self.num_points // 2):
             input1 = random.uniform(0, 1)
             input2 = random.uniform(0, 1)
-            
+
             if input1 == 0 and input2 == 0:
                 output_val = 1
             elif (input1 == 0 or input1 == 1) and (input2 == 0 or input2 == 1):
@@ -65,23 +66,6 @@ class NORGateDataset(Dataset):
             loaded_data = pickle.load(file)
         return loaded_data
 
-# Create the custom dataset
-custom_dataset = NORGateDataset(num_data_points)
-
-# Verification: Check if data was loaded or generated
-if os.path.exists(data_file):
-    print("Data loaded from file.")
-else:
-    print("Data generated and saved to file.")
-
-# Create a custom collate function for the DataLoader
-def custom_collate(batch):
-    inputs, outputs = zip(*batch)
-    return torch.tensor(inputs, dtype=torch.float32), torch.tensor(outputs, dtype=torch.float32)  # Use dtype=float32 for -1
-
-# Create a DataLoader with the custom collate function
-dataloader = DataLoader(custom_dataset, batch_size=batch_size, collate_fn=custom_collate)
-
 # Define the neural network architecture
 class StrictNORModel(nn.Module):
     def __init__(self, input_size):
@@ -94,37 +78,40 @@ class StrictNORModel(nn.Module):
         out = self.fc1(x)
         return out
 
-# Instantiate the model
-input_size = 2  # Number of input features (2 for NOR gate)
-model = StrictNORModel(input_size)
+# Create a custom collate function for the DataLoader
+def custom_collate(batch):
+    inputs, outputs = zip(*batch)
+    return torch.tensor(inputs, dtype=torch.float32), torch.tensor(outputs, dtype=torch.float32)  # Use dtype=float32 for -1
 
-# Define the loss function (Binary Cross-Entropy Loss) and optimizer (Adam)
-criterion = nn.BCEWithLogitsLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+# Function to evaluate the model
+def evaluate_model(model, dataloader):
+    model.eval()  # Set the model to evaluation mode
+    all_predictions = []
+    all_targets = []
 
-# Training parameters
-num_epochs = 10  # Number of training epochs
+    with torch.no_grad():
+        for inputs, targets in dataloader:
+            # Forward pass
+            outputs = model(inputs)
+
+            # Convert predictions to binary (0 or 1) using a threshold (e.g., 0.5)
+            predictions = (outputs >= 0.5).float()
+
+            all_predictions.extend(predictions.tolist())
+            all_targets.extend(targets.tolist())
+
+    accuracy = accuracy_score(all_targets, all_predictions)
+    return accuracy
 
 # Function to plot training loss
 def plot_training_loss(losses):
     plt.figure()
-    plt.plot(range(1, num_epochs+1), losses, marker='o', linestyle='-')
+    plt.plot(range(1, num_epochs + 1), losses, marker='o', linestyle='-')
     plt.xlabel('Epoch')
     plt.ylabel('Training Loss')
     plt.title('Training Loss Over Epochs')
     plt.grid(True)
     plt.show()
-
-# Function to evaluate the model
-def evaluate_model(model, dataloader):
-    # Evaluation code here
-    # Calculate and return evaluation metrics
-    pass
-
-# Function to load a pre-trained model
-def load_model(model, model_path):
-    model.load_state_dict(torch.load(model_path))
-    model.eval()  # Set the model to evaluation mode
 
 # Training loop function
 def train_model(model, dataloader, num_epochs, criterion, optimizer):
@@ -153,9 +140,32 @@ def train_model(model, dataloader, num_epochs, criterion, optimizer):
         # Append the average epoch loss to the list for plotting
         losses.append(avg_epoch_loss)
 
-        print(f"Epoch [{epoch+1}/{num_epochs}] Loss: {avg_epoch_loss:.4f}")
+        print(f"Epoch [{epoch + 1}/{num_epochs}] Loss: {avg_epoch_loss:.4f}")
 
     return losses
+
+# Create the custom dataset
+custom_dataset = NORGateDataset(num_data_points)
+
+# Verification: Check if data was loaded or generated
+if os.path.exists(data_file):
+    print("Data loaded from file.")
+else:
+    print("Data generated and saved to file.")
+
+# Create a DataLoader with the custom collate function
+dataloader = DataLoader(custom_dataset, batch_size=batch_size, collate_fn=custom_collate)
+
+# Instantiate the model
+input_size = 2  # Number of input features (2 for NOR gate)
+model = StrictNORModel(input_size)
+
+# Define the loss function (Binary Cross-Entropy Loss) and optimizer (Adam)
+criterion = nn.BCEWithLogitsLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+# Training parameters
+num_epochs = 10  # Number of training epochs
 
 # Call the training loop function
 losses = train_model(model, dataloader, num_epochs, criterion, optimizer)
@@ -164,10 +174,8 @@ losses = train_model(model, dataloader, num_epochs, criterion, optimizer)
 plot_training_loss(losses)
 
 # Call the evaluation function
-# evaluation_metrics = evaluate_model(model, validation_dataloader)
-
-# Load a pre-trained model if needed
-# load_model(model, "pretrained_model.pth")
+accuracy = evaluate_model(model, dataloader)
+print(f"Accuracy: {accuracy * 100:.2f}%")
 
 # Save the trained model
 torch.save(model.state_dict(), "strict_nor_model.pth")
